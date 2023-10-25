e5c1743c377a83f999db1bde7df54ac664c71cdc
felipemarianoferr <felipemf2@al.insper.edu.br>
2023-10-25 15:33:55 -0300

    DesenhaPcEpersonagem

--- classDesenhaCura.py
+++ classDesenhaCura.py
@@ -3,7 +3,12 @@ import pygame
 class Cura_pokemon():
     def __init__(self):
         self.img_box = pygame.transform.scale((pygame.image.load('imgBatalhas/barraVazia.png')),(640, 150))
+        self.img_pc = pygame.transform.scale((pygame.image.load('img/pokemonCenter.png')),(640, 400))
         self.fonte = pygame.font.Font('imgBatalhas/fontes.ttf', 20)
-    
+
+    def desenha_pc(self, window):
+        window.fill((0, 0, 0))
+        window.blit(self.img_pc, (0, 100))
+
     def desenha_box(self, window):
         window.blit(self.img_box, (0, 450))
